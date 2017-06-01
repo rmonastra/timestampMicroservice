47281145008f122245dd410276cec46393ec9bfd
rmonastra <rmonastra2@gmail.com>
2017-05-31 20:43:55 -0400

    configure for build

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
+});
+
+router.get('/:time', function(req, res) {
+
+    function unixToNaturalTime(unix) {
+        var date = new Date(unix * 1000);
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        var month = months[date.getMonth()];
+        var day = date.getDate();
+        var year = date.getFullYear();
+
+        var result = month + ' ' + day + ', ' + year;
+        return result;
+    }
+
+    if (!isNaN(req.params.time)) {
+        var result = unixToNaturalTime(req.params.time);
+        var data = { natural: req.params.time, unix: result };
+        res.json(data);
+    } else {
+        var naturalTime = new Date(req.params.time);
+        if (!isNaN(naturalTime)) {
+            var unix = naturalTime / 1000;
+            var data = { natural: req.params.time, unix: unix };
+            res.json(data);
+        } else {
+            res.json({ natural: null, unix: null });
+        }
+    }
 });
 
 module.exports = router;
